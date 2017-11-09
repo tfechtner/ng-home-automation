fd51b9c83b214d13f6a0cf30ee13595518946afc
Tom Fechtner <tom.fechtner@gmail.com>
2017-11-09 14:39:08 +0000

    Updated top nav with Sonos connection status

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SonosService } from '../../services/sonos.service';
+
 @Component({
-  selector: 'app-top-nav',
-  templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss']
+    selector: 'app-top-nav',
+    templateUrl: './top-nav.component.html',
+    styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+    connectedToSonos: boolean = null;
 
-  ngOnInit() {
-  }
+    constructor(private sonosService: SonosService) {
+
+    }
+
+    ngOnInit() {
+        // TODO: TopNavComponent - Subscribe to state updates from sonos service
+        setTimeout(() => {
+            this.connectedToSonos = this.sonosService.isConnected();
+        }, 0);
+    }
 
 }
