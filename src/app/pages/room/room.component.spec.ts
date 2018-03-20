04714510082cd204a4406df6a5efd2f360981329
Tom Fechtner <tom.fechtner@gmail.com>
2018-03-20 08:29:17 +0000

    Fixed Room component test

--- src/app/pages/room/room.component.spec.ts
+++ src/app/pages/room/room.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { PageService } from '../../services/page/page.service';
 import { RoomComponent } from './room.component';
 
 describe('RoomComponent', () => {
@@ -9,10 +10,9 @@ describe('RoomComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [RoomComponent],
-            providers: [
-                ActivatedRoute
-            ],
+            providers: [PageService]
         })
             .compileComponents();
     }));
