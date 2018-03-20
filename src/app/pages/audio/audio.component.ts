17910dbdb2bab3a59f254f1fc8dc243b67b469f3
Tom Fechtner <tom.fechtner@gmail.com>
2018-03-20 09:12:14 +0000

    Added Sonos service to Audio page

--- src/app/pages/audio/audio.component.ts
+++ src/app/pages/audio/audio.component.ts
@@ -1,20 +1,39 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import {PageService} from '../../services/page/page.service';
+import { PageService } from '../../services/page/page.service';
+import { SonosService } from '../../services/sonos/sonos.service';
+import { SonosServiceMock } from '../../services/sonos/sonos.service.mock';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-audio',
     templateUrl: './audio.component.html',
-    styleUrls: ['./audio.component.scss']
+    styleUrls: ['./audio.component.scss'],
+    providers: [ SonosServiceMock ]
 })
-export class AudioComponent implements OnInit {
+export class AudioComponent implements OnInit, OnDestroy {
+
+    subscriptions: Array<Subscription> = [];
 
     constructor(
-        private pageService: PageService
+        private pageService: PageService,
+        private sonosService: SonosServiceMock
     ) {}
 
     ngOnInit() {
         this.pageService.setPageTitle('Audio');
+        this.sonosService.getState().subscribe((state) => {
+            // Write adaptor
+            console.log(state);
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.subscriptions.length > 0) {
+            for (let subscription of this.subscriptions) {
+                subscription.unsubscribe();
+            }
+        }
     }
 
 }
