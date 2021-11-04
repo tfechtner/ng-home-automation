967900b7c5f4ffe10c6a165ed808a728910663b0
Tom Fechtner <tom.fechtner@gmail.com>
2021-11-04 18:24:34 +0000

    Settings reload button

---
+++ frontend/src/app/components/update/update.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CheckForUpdateService } from '@services/update/update.service';
+import packageJson from '../../../../package.json';
+
+@Component({
+    selector: 'ngx-update',
+    templateUrl: './update.component.html',
+    styleUrls: ['./update.component.scss']
+})
+export class UpdateComponent implements OnInit {
+
+    public version = packageJson.version;
+    public isServiceWorkerEnabled: boolean = null;
+
+    constructor(
+        private _checkForUpdateService: CheckForUpdateService,
+    ) {}
+
+    ngOnInit(): void {
+        this.isServiceWorkerEnabled = this._checkForUpdateService.isServiceWorkerEnabled()
+    }
+
+    onClickReload() {
+        this._checkForUpdateService.reload();
+    }
+
+}
--- frontend/src/app/pages/settings/settings.module.ts
+++ frontend/src/app/pages/settings/settings.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { NbButtonModule, NbCardModule, NbIconModule } from '@nebular/theme';
 import { ThemeModule } from '../../@theme/theme.module';
 import { ThemeComponent } from '../../components/theme/theme.component';
+import { UpdateComponent } from '../../components/update/update.component';
 import { SettingsComponent } from './settings.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SettingsComponent } from './settings.component';
     ],
     declarations: [
         SettingsComponent,
-        ThemeComponent
+        ThemeComponent,
+        UpdateComponent
     ]
 })
 export class SettingsModule {
