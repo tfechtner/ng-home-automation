af5554fa34b69ef070a1213edddb281d813cf86d
Tom Fechtner <tom.fechtner@gmail.com>
2022-01-12 20:01:15 +0000

    FE server SSL

--- frontend-server/src/server.ts
+++ frontend-server/src/server.ts
@@ -1,13 +1,20 @@
 'use strict';
+import fs from 'fs';
+import https from 'https';
 import express from 'express';
 import compression from 'compression';
 
 const ip = process.env.NODE_ENV === 'production' ? '192.168.0.44' : '192.168.0.32';
-const port = 4100;
+const port = 443;
 const appFolder = 'frontend-server/www';
 
 const app = express();
 app.use(compression());
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync('./frontend-server/certs/server.key'),
+    cert: fs.readFileSync('./frontend-server/certs/server.crt'),
+}, app);
 
 // ---- SERVE STATIC FILES ---- //
 app.use(express.static(appFolder, {maxAge: '1y'}));
@@ -18,7 +25,7 @@ app.all('*', (req, res) => {
 });
 
 // ---- START UP THE NODE SERVER  ----
-app.listen(port, ip, () => {
+httpsServer.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log('Node Express server for ' + app.name + ' listening on http://:' + ip + ':' + port);
+    console.log('Node Express server for ' + app.name + ' listening on https://:' + ip + ':' + port);
 });
